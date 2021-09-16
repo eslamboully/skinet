7bc83892685e0ad31fe5366001023d7dd1998c47
Abdelrahman Osama <eslamboully@gmail.com>
2021-09-16 13:11:52 +0200

    fix product specification param

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -10,13 +10,13 @@ namespace Core.Specifications
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
-        public int? BrandId { get; set; }
-        public int? TypeId { get; set; }
-        public string Sort { get; set; }
         private string _search;
         public string Search { 
             get => _search;
             set => _search = value.ToLower();
         }
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string Sort { get; set; }
     }
 }
