12123144a21f42a1e86fb0f338d05caa9d2ac454
Abdelrahman Osama <eslamboully@gmail.com>
2021-09-17 14:07:28 +0200

    angular client project

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './Models/pagination';
+import { IProduct } from './Models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'client';
+  products: IProduct[]; 
+
+  constructor (private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+      this.products = response.data;
+    },error => {
+      console.log(error);
+    });
+  }
+
+}
